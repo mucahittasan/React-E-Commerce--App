f7812456edbfe489a78b1b0ef7ed754402d448c2
Mücahit <mucahittasan00@gmail.com>
2022-04-24 02:13:07 +0300

    Footer component added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 // Modules
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 // Components
-import Home from './pages/Home';
-import Basket from './pages/Basket';
-import ItemDetail from './pages/ItemDetail';
-import Error404 from './pages/Error404';
-
+import Home from "./pages/Home";
+import Basket from "./pages/Basket";
+import ItemDetail from "./pages/ItemDetail";
+import Error404 from "./pages/Error404";
+import Footer from "./components/Footer";
 
 const App = () => {
-
   return (
-    <div>
+    <div className="main">
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/basket" element={<Basket />} />
+          <Route path="/item/:item_id" element={<ItemDetail />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </div>
 
-      <Routes>
-        <Route path='/' element={ <Home /> } />
-        <Route path='/basket' element={ <Basket /> } />
-        <Route path='/item/:item_id' element={ <ItemDetail /> } />
-        <Route path='*' element={ <Error404 /> } />
-      </Routes>
-
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,56 @@
 import React from "react";
+import {
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillTwitterCircle,
+  AiOutlineInstagram,
+} from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="copy">
+        Designed by{" "}
+        <a
+          href="https://github.com/mucahittasan"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Mucahit Tasan
+        </a>
+      </div>
+      <div className="social">
+        <a
+          href="https://linkedin.com/in/mucahittasan"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <AiFillLinkedin />
+        </a>
+        <a
+          href="https://github.com/mucahittasan"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <AiFillGithub />
+        </a>
+        <a
+          href="https://twitter.com/MurattTasan"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <AiFillTwitterCircle />
+        </a>
+        <a
+          href="https://instagram.com/tasanmucahit"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <AiOutlineInstagram />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
