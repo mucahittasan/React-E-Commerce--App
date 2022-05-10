5c57eec5f003cb1cf9845991c392a5876556793a
mucahittasan <mucahittasan00@gmail.com>
2022-05-10 18:15:10 +0300

    Footer fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 // Modules
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 // Images
 import basketLogo from "../images/basket.png";
@@ -7,31 +7,21 @@ import basketLogo from "../images/basket.png";
 import DataContext from "../context/DataContext";
 
 const Header = () => {
-  const { basket, items, addItem, removeItem } = useContext(DataContext);
-
-  const [search, setSearch] = useState("");
-
-  const filteredSearch = items.filter((item) =>
-    item.title.toLowerCase().includes(search)
-  );
-
-  console.log(filteredSearch);
+  const { basket } = useContext(DataContext);
 
   return (
     <header className="header">
-      <div className="header-content">
-        <Link to="/">
-          <h1 className="logo">
-            Steal <span> im </span>
-          </h1>
-        </Link>
+      <Link to="/">
+        <h1 className="logo">
+          Steal <span> im </span>
+        </h1>
+      </Link>
 
-        <div className="basket">
-          <Link to="/basket">
-            <img src={basketLogo} alt="" />
-            {basket.length > 0 ? <span> {basket.length} </span> : null}
-          </Link>
-        </div>
+      <div className="basket">
+        <Link to="/basket">
+          <img src={basketLogo} alt="" />
+          {basket.length > 0 ? <span> {basket.length} </span> : null}
+        </Link>
       </div>
     </header>
   );
